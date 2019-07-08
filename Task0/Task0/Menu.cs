1dc7602e373fe63e479df29b8f6a0a806ffe231d
DariaYudina <>
2019-07-08 14:37:17 +0400

    Remarks

--- Task0/Task0/Menu.cs
+++ Task0/Task0/Menu.cs
@@ -11,11 +11,11 @@ namespace Task0
             string ex = "yes";
             while (ex == "yes")
             {
-                Console.WriteLine("Меню:\n");
-                Console.WriteLine("1: Sequence\n" +
-                                  "2: Simple\n" +
-                                  "3: Square\n" +
-                                  "4: Выйти\n");
+                Console.WriteLine("Menu:\n");
+                Console.WriteLine("1: Sequence" + Environment.NewLine +
+                                  "2: Simple" + Environment.NewLine +
+                                  "3: Square" + Environment.NewLine +
+                                  "4: Выйти" + Environment.NewLine);
                 string value = Console.ReadLine();
                 int menuitem;
                 bool success = Int32.TryParse(value, out menuitem);
@@ -39,9 +39,9 @@ namespace Task0
                             {
                                 try
                                 {
-                                Console.WriteLine("Результат:");
-                                if (Task0.Simple(validation())) { Console.WriteLine("Введенное число - простое"); }
-                                else { Console.WriteLine("Введенное число - не является простым"); };
+                                Console.WriteLine("Result:");
+                                if (Task0.Simple(validation())) { Console.WriteLine("the number is simple"); }
+                                else { Console.WriteLine("the number is not simple"); };
                                 }
                                 catch (Exception e)
                                 {
@@ -61,21 +61,21 @@ namespace Task0
                                 }
                                 break;
                             }
-                        case 4: Console.WriteLine("Вы выбрали выйти\n"); ex = "no"; break;
-                        default: Console.WriteLine("Выберите существующий пункт меню"); break;
+                        case 4: Console.WriteLine("Exit" + Environment.NewLine); ex = "no"; break;
+                        default: Console.WriteLine("Select an existing menu item"); break;
                     }
                 }
-                else { Console.WriteLine("Вы ввели не число "); }
+                else { Console.WriteLine("You entered not a number"); }
             }
         }
         public int validation()
         {
-            Console.WriteLine("Введите число для выбранной функции:\n");
+            Console.WriteLine("Enter a number:\n");
             string value = Console.ReadLine();
             int number;
             bool success = Int32.TryParse(value, out number);
             if (success) { return number; }
-            else { throw new Exception("Вы ввели не число"); }
+            else { throw new Exception("You entered not a number"); }
         }       
     }
 }
