80fd666252637b89a5cb309d7164d264e422b7af
dashaudina06 <dashaudina06@gmail.com>
2019-07-26 03:02:35 +0400

    Task 3_3 add classes

---
+++ Task3/Task3_3/DynamicArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_3
+{
+    class DynamicArray<T> : IList<T>, ICollection<T>, IEnumerable<T>
+    {
+        private T[] array;
+        private int length;
+        public DynamicArray(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException("capacity", "capacity must be greater than 0");
+            }
+            array = new T[capacity];
+        }
+        public DynamicArray()
+           : this(8)
+        {
+        }
+
+    }
+}
