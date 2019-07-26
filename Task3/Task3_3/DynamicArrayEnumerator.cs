e17eb1531d2c582ac151966126ca6e46e1ac4fea
dashaudina06 <dashaudina06@gmail.com>
2019-07-26 12:32:35 +0400

    Task 3 add dynamic array

---
+++ Task3/Task3_3/DynamicArrayEnumerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_3
+{
+    internal class DynamicArrayEnumerator<T> : IEnumerator<T>
+    {
+        private int position = -1;
+        private DynamicArray<T> dynamicArray;
+
+        public DynamicArrayEnumerator(DynamicArray<T> dynamicArray)
+        {
+            this.dynamicArray = dynamicArray;
+        }
+
+        public T Current
+        {
+            get { return this.dynamicArray[position]; }
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < this.dynamicArray.Count);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+
+        bool IEnumerator.MoveNext()
+        {
+            return MoveNext();
+        }
+
+        void IEnumerator.Reset()
+        {
+            Reset();
+        }
+
+        T IEnumerator<T>.Current
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        object IEnumerator.Current
+        { get { return Current; } }
+
+        void IDisposable.Dispose()
+        {
+        }
+    }
+}
