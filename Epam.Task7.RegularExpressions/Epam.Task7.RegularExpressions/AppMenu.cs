c3c60fb8a1685469e770cc44b5745c65009e52d9
dashaudina06 <dashaudina06@gmail.com>
2019-08-30 22:05:35 +0400

    Task7 add

---
+++ Epam.Task7.RegularExpressions/Epam.Task7.RegularExpressions/AppMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Epam.Task7.RegularExpressions
+{
+    public class AppMenu
+    {
+        public static 
+            bool ContainsDate(string str)
+        {
+            Match match = Regex.Match(str, @"\d{2}[-]\d{2}[-]\d{4}");
+            return match.Success;
+        }
+        public static string TagReplace(string str)
+        {
+            string pattern = @"<[^>]*>";
+            return new Regex(pattern).Replace(str, "_");
+        }
+    }
+}
---
+++ Epam.Task7.RegularExpressions/Epam.Task7.RegularExpressions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Epam.Task7.RegularExpressions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Epam.Task7.RegularExpressions")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("702852ac-32f9-4e9e-89e2-b34046f4f1a4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
