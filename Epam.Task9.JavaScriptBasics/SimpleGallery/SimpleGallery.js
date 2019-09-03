78f3c673163a51cd2c022fe38a64f230391cc83d
dashaudina06 <dashaudina06@gmail.com>
2019-09-03 22:56:14 +0400

    Task 9 done

--- Epam.Task9.JavaScriptBasics/SimpleGallery/SimpleGallery.js
+++ Epam.Task9.JavaScriptBasics/SimpleGallery/SimpleGallery.js
@@ -2,10 +2,19 @@ const path = "file:///C:/Users/Daria/Desktop/EPAM/XT_Q2/Epam.Task9.JavaScriptBas
 const regex = /\d+.(html)/;
 
 let div = document.getElementById("timer");
-let currentTime = 2;
+let currentTime = 5;
 
 const pages = ["1.html", "2.html", "3.html"];
 currentPageIndex = pages.indexOf(regex.exec(window.location.href)[0]);
+if (currentPageIndex === pages.length - 1) {
+    let isRestart = confirm("Press OK if you want to restart, Cancel if not");
+    console.log(isRestart);
+    if (isRestart) {
+        window.location.href = path + pages[0];
+    } else {
+        window.close();
+    }
+}
 let interval = setInterval(startTimer, 1000);
 
 let stopBtn = document.getElementById("stop");
@@ -16,6 +25,8 @@ stopBtn.addEventListener("click", () => {
 
 let goBtn = document.getElementById("go");
 goBtn.addEventListener("click", () => {
+    currentTime = 5;
+    div.innerHTML = currentTime;
     interval = setInterval(startTimer, 1000);
 })
 
@@ -31,4 +42,18 @@ function startTimer() {
             window.location.href = path + pages[0];
         }
     }
-}   
+}
+
+let nextBtn = document.getElementById("next");
+nextBtn.addEventListener("click", () => {
+    if (currentPageIndex !== pages.length - 1) {
+        window.location.href = path + pages[currentPageIndex + 1];
+    }
+})
+
+let prevBtn = document.getElementById("prev");
+prevBtn.addEventListener("click", () => {
+    if (currentPageIndex !== 0) {
+        window.location.href = path + pages[currentPageIndex - 1];
+    }
+})
