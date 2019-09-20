1359f2b5c38b63cdc1c907906dac32955bac928d
dashaudina06 <dashaudina06@gmail.com>
2019-09-20 17:56:37 +0400

    Task 10 add

---
+++ Epam.Task10.WebPages/Epam.WebPages.UIweb/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using Epam.WebPages.Common;
+using Epam.WebPages.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epam.WebPages.UIweb.Models
+{
+    public static class UserModel
+    {
+        public static IEnumerable<User> GetAllUsers()
+        {
+            return DependencyResolver.UsersLogic.GetAllUsers();
+        }
+    }
+}
